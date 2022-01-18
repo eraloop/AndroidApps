97b38c4626cad9b150c9bfd9511d438f964223d9
era <romanricakam@gmail.com>
2022-01-18 17:58:48 +0100

    done with the layout and now trying to setuo the logic of the application,

--- SendMessageApp/app/src/main/java/com/example/sendmessageapp/MainActivity.java
+++ SendMessageApp/app/src/main/java/com/example/sendmessageapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.sendmessageapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+     EditText phoneView = findViewById(R.id.phone);
+    //CharSequence phone = phoneView.getText();
+
+    EditText messageView = findViewById(R.id.message);
+    //CharSequence message =  phoneView.getText();
+
+    TextView handle = findViewById(R.id.displaybox);
+
+    String combinedMessage = "'from Marry\n'+" + messageView;
+
+    public void sendMessage(View v){
+        handle.setText(combinedMessage);
     }
 }
