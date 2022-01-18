0227813860a52c9033b9c0047747434a0b60c139
era <romanricakam@gmail.com>
2022-01-18 14:46:28 +0100

    updating , adding onclicklistener

--- StopWatchApp/app/src/main/java/com/example/stopwatchapp/MainActivity.java
+++ StopWatchApp/app/src/main/java/com/example/stopwatchapp/MainActivity.java
@@ -2,39 +2,57 @@ package com.example.stopwatchapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
-   int minutes;
-   // int seconds;
-   // int count = 1;
+            //start of new type of code, use of onclicklistener
 
-   TextView time = findViewById(R.id.time_box);
+            Button start =   findViewById(R.id.start);
+            Button stop =  findViewById(R.id.stop);
+            Button reset =   findViewById(R.id.reset);
 
-   public void Start(View v){
-       for(int i = 0; i <= 10; i ++){
-           if(i == 10){
-               i = 0;
-           }
-           minutes ++ ;
-           time.setText(minutes);
+            start.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                  // Start(v);
+                }
+            });
+
+            stop.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                   //Stop(v);
+                }
+            });
+
+            reset.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    //Reset(v);
+                }
+            });
+        }
+
+        //TextView time = findViewById(R.id.timebox);
+
+      /* public void Start(View v){
+
        }
-    }
 
-    public void Stop(View v){
-        //count =  2;
-    }
+        public void Stop(View v){
+            //count =  2;
+        }
 
-   public void Reset(View v){
+       public void Reset(View v){
 
-    }
+        }*/
+
 }
